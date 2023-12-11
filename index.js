c127d622427b92786780550977ca92b9eb4b5de1
Bharathkumar <bharath91505@gmail.com>
2023-12-11 22:12:50 +0530

    updated in

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
 const commentRoutes = require("./routes/comment");
 const multer = require("multer");
 const path = require("path");
-
+dotenv.config();
 
 app.use(cors({
   origin: 'https://blogapppro.netlify.app',
- 
 }));
-dotenv.config();
 app.use(express.json());
+app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 
@@ -47,8 +45,12 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
 app.use('/api/comments', commentRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 app.listen("5000", () => {
   console.log("Backend is running.");
