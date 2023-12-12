5fb08f6516229e6990560b82921bdc372d6a8b8b
Bharathkumar <bharath91505@gmail.com>
2023-12-12 08:37:45 +0530

    re

--- index.js
+++ index.js
@@ -11,22 +11,12 @@ const multer = require("multer");
 const path = require("path");
 dotenv.config();
 
-const imagesPath = path.join(__dirname, "images");
 app.use(cors({
   origin: 'https://blogapppro.netlify.app',
 }));
 app.use(express.json());
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-
-if (process.env.NODE_ENV === 'development') {
-  app.use("/images", express.static(path.join(__dirname, "/images")));
-}
-
-// Serve static images in production
-if (process.env.NODE_ENV === 'production') {
-  app.use("/images", express.static(imagesPath));
-}
 
 
 mongoose.connect(process.env.MONGO_URL, {
